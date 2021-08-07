ec837cec96581bbe18ec6072755935d7455b011a
Jewcub <52448067+Jewcub@users.noreply.github.com>
2021-08-07 14:44:45 +0800

    show something

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,7 +1,13 @@
 import './App.css';
+import Login from './views/Login';
 
 const App: React.FC = (props) => {
-  return <div className="App">{props.children}</div>;
+  return (
+    <div className="App">
+      <Login></Login>
+      {props.children}
+    </div>
+  );
 };
 
 export default App;
