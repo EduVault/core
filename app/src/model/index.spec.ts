dc2d1a5acccc75e0905a0e422d371b3b6a3cd052
Jewcub <52448067+Jewcub@users.noreply.github.com>
2021-09-23 17:20:02 +0800

    use mock server

--- app/src/model/index.spec.ts
+++ app/src/model/index.spec.ts
@@ -1,4 +1,6 @@
 import { eduvault } from './';
-it('loads eduvault and mock server', () => {
-  expect(eduvault.api.ping()).toBeTruthy();
-});
+
+it('loads eduvault and mock server', async () => {
+  const pingRes = await eduvault.api.ping();
+  expect(pingRes).toBe(true);
+}, 20000);
--- app/src/setupTests.ts
+++ app/src/setupTests.ts
@@ -3,3 +3,8 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+import { mockServer as server } from '@eduvault/sdk-js';
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
