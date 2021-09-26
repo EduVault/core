98b12c5ffb35e2e82a25f7a2cbe8d50d67330f6f
Jewcub <52448067+Jewcub@users.noreply.github.com>
2021-09-26 10:42:43 +0800

    query helper

--- api/src/helpers/api.ts
+++ api/src/helpers/api.ts
@@ -23,3 +23,21 @@ export const respondError = (res: Response, error: ErrorKey, raw?: any) => {
   res.status(errors[error].status || 500);
   res.json(response);
 };
+
+/**
+ * @description makes an object from a query string
+ * @example 'param1=asdf&param2=asdf' => { param1: 'asdf', param2: 'asdf'}
+ */
+ export const parseQueries = (queries: string) => {
+  const parsed = Object.fromEntries(new URLSearchParams(queries));
+  return parsed;
+};
+
+/**
+ * @description makes a query string from an object
+ * @example { param1: 'asdf', param2: 'asdf'} => 'param1=asdf&param2=asdf'
+ */
+export const formatQueries = (data: any)=>{
+  const queries = new URLSearchParams(Object.entries(data)).toString();
+  return queries
+}
