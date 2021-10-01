0ce27183058fead50d0512a85e24d17b35541f66
Jewcub <52448067+Jewcub@users.noreply.github.com>
2021-10-01 21:06:57 +0800

    rename calls to methods

---
+++ api/src/db/methods.ts
@@ -0,0 +1,23 @@
+import { Database } from '@textile/threaddb';
+import { IPerson } from '../models/person';
+import { IApp } from '../types';
+
+export const findAppByID = async (db: Database, id: string) =>
+  (await db.collection<IApp>('app').findById(id)) as IApp;
+
+export const findPersonByID = async (db: Database, id: string) =>
+  (await db.collection<IPerson>('person').findById(id)) as IPerson;
+
+export const findPersonByUsername = async (db: Database, username: string) =>
+  (await db.collection<IPerson>('person').findOne({ username })) as IPerson;
+
+export const savePerson = async (db: Database, person: IPerson) =>
+  await db.collection<IPerson>('person').save(person);
+
+export const saveApp = async (db: Database, app: IApp) =>
+  await db.collection<IApp>('app').save(app);
+
+export const findAllAppsWithAuthorizedDomains = async (db: Database) =>
+  await db
+    .collection<IApp>('app')
+    .find({ authorizedDomains: { $exists: true } });
