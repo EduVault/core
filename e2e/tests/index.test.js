b3da8452d2cd4ea2f9815519531843871148f2b7
Jewcub <52448067+Jewcub@users.noreply.github.com>
2021-10-04 00:21:46 +0800

    e2e

--- e2e/tests/index.test.js
+++ e2e/tests/index.test.js
@@ -9,7 +9,7 @@ describe('Index Page', () => {
   it('should contain a login element', () => {
     cy.visit('/app/login');
 
-    const login = cy.contains('Continue with Password');
+    const login = cy.contains('Login with EduVault');
     login.should('exist');
     // login.get('input#username').should('exist');
     // login.get('input#password').should('exist');
