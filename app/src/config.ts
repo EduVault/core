c2b2a31a15461d4fa15aae03dc85d6b73e107212
Jewcub <52448067+Jewcub@users.noreply.github.com>
2021-10-05 16:30:59 +0800

    put back CI after app build change

--- app/src/config.ts
+++ app/src/config.ts
@@ -2,15 +2,15 @@ const env = process.env;
 const HOST = env.REACT_APP_HOST;
 
 export const URL_API =
-  process.env.NODE_ENV === 'production'
+  env.NODE_ENV === 'production'
     ? `https://${HOST}/api`
-    : process.env.REACT_APP_SUPPRESS_MSW === 'true'
+    : env.REACT_APP_SUPPRESS_MSW === 'true'
     ? 'https://localhost:8082/api'
     : undefined;
 
 export const URL_WS_API =
-  process.env.NODE_ENV === 'production'
+  env.NODE_ENV === 'production'
     ? `wss://${HOST}/api/ws`
     : 'wss://localhost:8082/api/ws';
 
-console.log({ env, HOST, URL_API, URL_WS_API });
+// console.log({ env, HOST, URL_API, URL_WS_API });
