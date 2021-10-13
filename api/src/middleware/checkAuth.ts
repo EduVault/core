0de1a2be5c719d80c4bd34a317e70797a9d83363
Jewcub <52448067+Jewcub@users.noreply.github.com>
2021-10-13 17:51:52 +0800

    print cookie

--- api/src/middleware/checkAuth.ts
+++ api/src/middleware/checkAuth.ts
@@ -3,6 +3,7 @@ import { Request, Response, NextFunction } from 'express';
 import { respondError } from '../helpers';
 
 export const checkAuth = (req: Request, res: Response, next: NextFunction) => {
+  console.log('cookies', req.headers.cookie);
   if (!req.isAuthenticated()) {
     // console.log('authenticationFailed');
     respondError(res, 'authenticationFailed');
