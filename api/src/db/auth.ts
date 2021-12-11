5924f1aa07c8d8f012bbcd1e6992ae3e951c58b2
Jewcub <xiaokourou@gmail.com>
2021-12-11 11:53:33 +0800

    remove unused

--- api/src/db/auth.ts
+++ api/src/db/auth.ts
@@ -1,6 +1,5 @@
-import { createAPISig, UserAuth as PersonAuth, PrivateKey } from '@textile/hub';
+import { createAPISig } from '@textile/hub';
 import { TEXTILE_USER_API_KEY } from '../config';
-import { newClientDB } from './init';
 
 /** @param seconds (300) time until the sig expires */
 export const getAPISig = async (seconds: number = 300) => {
@@ -14,32 +13,4 @@ export const getAPISig = async (seconds: number = 300) => {
     console.log(err.message);
     throw err;
   }
-};
-
-export const localChallengeHandler = (id: PrivateKey) => {
-  const challengeFunc = async (challenge: Uint8Array): Promise<Uint8Array> => {
-    return await id.sign(challenge);
-  };
-  return challengeFunc;
-};
-
-export const generatePersonAuth = async (
-  pubkey: string,
-  challengeHandler: (challenge: Uint8Array) => Uint8Array | Promise<Uint8Array>
-): Promise<PersonAuth> => {
-  const db = await newClientDB();
-  const token = await db.getTokenChallenge(pubkey, challengeHandler);
-  const signature = await getAPISig();
-  return {
-    ...signature,
-    token: token,
-    key: TEXTILE_USER_API_KEY,
-  } as PersonAuth;
-};
-
-export const localPersonAuth = async (id: PrivateKey) => {
-  return await generatePersonAuth(
-    id.public.toString(),
-    localChallengeHandler(id)
-  );
 };
