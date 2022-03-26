b755f1ec35006912dbe1207c6a4606555e26a7ce
jacobcoro <xiaokourou@gmail.com>
2022-03-26 13:11:39 +0800

    bump cypress, add back login page e2e test

--- e2e/tests/index.test.js
+++ e2e/tests/index.test.js
@@ -1,18 +1,24 @@
 /// <reference types="Cypress" />
 
 describe('Index Page', () => {
-  it('should be able to ping server', async () => {
-    const response = await cy.request('/api/ping');
-    expect(response).to.have.property('status', 200);
-    expect(response.body).to.have.property('content', 'pong');
-  });
+  // it('should be able to ping server', async () => {
+  //   const response = await cy.request('/api/ping');
+  //   expect(response).to.have.property('status', 200);
+  //   expect(response.body).to.have.property('content', 'pong');
+  // });
   it('should contain a login element', () => {
-    cy.visit('/app/login');
+    cy.visit('/login');
 
-    const login = cy.contains('Login with EduVault');
-    login.should('exist');
-    // login.get('input#username').should('exist');
-    // login.get('input#password').should('exist');
-    // login.get('button[type="submit"]').should('exist');
+    cy.contains('Login with EduVault');
+    cy.get('#eduvault-button').click();
+
+    cy.get('button:contains("Continue with Password")').should('be.disabled');
+    const emailInput = cy.get('input[type=email]').should('exist');
+    emailInput.type('123@123.123');
+    const passwordInput = cy.get('input[type=password]').should('exist');
+    passwordInput.type('123@123.123');
+    cy.get('button:contains("Continue with Password")')
+      .should('not.be.disabled')
+      .click();
   });
 });
